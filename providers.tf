b061c14f6134d73bc9de65a6482bf39f0b648fba
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-12 14:07:03 -0500

    providers

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aviatrix = {
+      source  = "aviatrixsystems/aviatrix"
+      version = "~> 3.0.0"
+    }
+    equinix = {
+      source = "equinix/equinix"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
