e9c423d07e91c51f75056af02c3407bf11bb1871
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-27 12:10:19 -0500

    more host

--- ne_intermediary.tf
+++ ne_intermediary.tf
@@ -88,7 +88,7 @@ resource "local_file" "intermediary_config" {
 resource "ansible_host" "ne_intermediary" {
   count = var.edge["intermediary_type"] == "network-edge" ? 1 : 0
 
-  name   = "ne_intermediary"
+  name   = one(equinix_network_device.ne_intermediary).ssh_ip_fqdn
   groups = ["eqx"]
 
   variables = {
